17121b636ee5b9c687e6375d733e59c1ce658fbc
Piyush <psantwani@gmail.com>
2017-02-18 14:28:46 +0530

    mongoDB connection and config.html

--- index.js
+++ index.js
@@ -1,55 +1,40 @@
-var config = require('./config.js');
-var flock = require('flockos');
 var express = require('express');
-var store = require('./store.js');
 var fs = require('fs');
 var util = require('util');
 var Mustache = require('mustache');
+var nunjucks  = require('nunjucks');
 
-flock.appId = config.appId;
-flock.appSecret = config.appSecret;
+global.flock = require('flockos');
+global.assert = require('assert');
+global.app = express();
 
-var app = express();
+var flockConfig = require('./config/flock_config');
+var mongoConfig = require('./config/mongo_config');
+var mongoOperations = require('./app/db/mongo_operations');
+
 app.use(flock.events.tokenVerifier);
-app.post('/events', flock.events.listener);
-
+app.use(express.static(`${__dirname}/public`));
+app.set('view engine', 'html');
 app.listen(8080, function () {
     console.log('Listening on 8080');
 });
 
-flock.events.on('app.install', function (event, callback) {
-    store.saveToken(event.userId, event.token);
-    callback();
+mongoConfig.connect(function(db){
+	mongoOperations.setDBInstance(db);
 });
 
-//var messageTemplate = require('fs').readFileSync('message.mustache.flockml', 'utf8');
-flock.events.on('client.pressButton', function (event, callback) {
-	//var flockml = Mustache.render(messageTemplate, { event: event, widgetURL: 'https://drawingboard-qqtudaqxcu.now.sh/' });
-//     flock.callMethod('chat.sendMessage', store.getToken(event.userId), {
-// 	  to: event.chat,
-// 	  text: "Here is your server",
-// 	  attachments: [{
-//     "description": "drawing board",
-//     "views": {
-//         "widget": { "src": "https://drawingboard-qqtudaqxcu.now.sh/", "width": 400, "height": 400 }
-//     },
-//     "buttons": [{
-//         "name": "Send",
-//         "action": { "type": "openWidget", "desktopType": "modal", "mobileType": "modal", "url": "" },
-//         "id": "Send"
-//     }, {
-//         "name": "Cancel",
-//         "action": { "type": "openWidget", "desktopType": "sidebar", "mobileType": "modal", "url": "" },
-//         "id": "Cancel"
-//     }]
-// }]
-// 	}, 
-// 	function (error, response) {
-// 		console.log("Send message done: " + JSON.stringify(response));
-// 		console.log("error: " + error + JSON.stringify(error));
-// 	if (!error) {
-// 		console.log(response);
-// 	}
-// 	callback();
-// 	});
+flock.appId = flockConfig.appId;
+flock.appSecret = flockConfig.appSecret;
+
+var flockEvents = require('./app/flock/events');
+var flockMethods = require('./app/flock/methods');
+
+nunjucks.configure(`${__dirname}/views`, {
+  autoescape: true,
+  express   : app
+});
+
+app.post('/events', flock.events.listener);
+app.get('/config', function(req, res){
+	res.render('config');
 });
